9ef89a58085390ca16bae53d08cefe3ff470b4d9
Lucas <lucas.souza.24@cjr.org.br>
2025-07-03 16:44:56 -0300

    mudando o escopo para receber vetor de filas

--- gerenciador_de_processos/escalonador.c
+++ gerenciador_de_processos/escalonador.c
@@ -8,6 +8,6 @@
 // OUTRAS FILAS DEVEM UTILIZAR ROUND ROBIN COM QUANTUM DE 1 MILISEGUNDO
 // PROCESSOS DE OUTRAS FILAS DEVEM PODER SOFRER AGING E/OU DECAYING
 
-void escalonador(Fila* fila_tempo_real, Fila* fila_usuario_1, Fila* fila_usuario_2, Fila* fila_usuario_3){
+void escalonador(Fila* filas[]){
     
 }
