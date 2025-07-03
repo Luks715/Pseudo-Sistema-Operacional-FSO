59d547e9cc22ecd672c1e83253fdb70eea61976c
Lucas <lucas.souza.24@cjr.org.br>
2025-07-03 00:17:01 -0300

    escalonador de processos

--- gerenciador_de_processos/escalonador.c
+++ gerenciador_de_processos/escalonador.c
@@ -1,6 +1,13 @@
-#include "fila.h"
-#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-bool escalonador(){
+#include "fila.h"
+
+// FIFO DEVE SER UTILIZADO PARA A FILA DE TEMPO REAL
+
+// OUTRAS FILAS DEVEM UTILIZAR ROUND ROBIN COM QUANTUM DE 1 MILISEGUNDO
+// PROCESSOS DE OUTRAS FILAS DEVEM PODER SOFRER AGING E/OU DECAYING
+
+void escalonador(Fila* fila_tempo_real, Fila* fila_usuario_1, Fila* fila_usuario_2, Fila* fila_usuario_3){
     
 }
