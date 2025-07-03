227f831a03d37853e01692e348aafa16f4210aa8
Lucas <lucas.souza.24@cjr.org.br>
2025-07-03 00:16:26 -0300

    header da função escalonador

---
+++ gerenciador_de_processos/escalonador.h
@@ -0,0 +1,8 @@
+#ifndef ESCALONADOR_H
+#define ESCALONADOR_H
+
+#include "fila.h"
+
+void escalonador(Fila* fila_tempo_real, Fila* fila_usuario_1, Fila* fila_usuario_2, Fila* fila_usuario_3);
+
+#endif
