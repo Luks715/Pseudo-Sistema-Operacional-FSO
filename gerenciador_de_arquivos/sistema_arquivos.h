5780cbac51a8a06c7d2f9b9bc9933b89d8522f3e
Lucas <lucas.souza.24@cjr.org.br>
2025-07-09 08:13:47 -0300

    adicionando argumento index_arquivo na header

--- gerenciador_de_arquivos/sistema_arquivos.h
+++ gerenciador_de_arquivos/sistema_arquivos.h
@@ -5,6 +5,6 @@
 #include "../include/processo.h"
 
 int sistema_arquivos(int operacao, char nome_arquivo, int tamanho_bloco, Disco* disco, Processo* processo);
-Arquivo* buscar_arquivo(char nome_arquivo, Disco* disco);
+Arquivo* buscar_arquivo(char nome_arquivo, Disco* disco, int* index_arquivo);
 
 #endif
